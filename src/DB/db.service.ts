d8a510a7003ee96249ff07566b6d7d500f17420d
Polishchuk <s.polishchuk@amiga.agency>
2023-07-31 00:27:51 +0500

    feat: implement track endpoint

--- src/DB/db.service.ts
+++ src/DB/db.service.ts
@@ -1,5 +1,7 @@
+import { Track } from 'src/track/interfaces/track.interface';
 import { User } from 'src/user/interfaces/user.interface';
 
 export class DB {
   users: User[] = [];
+  tracks: Track[] = [];
 }
--- src/track/track.controller.ts
+++ src/track/track.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
-
+import { CreateTrackDto, UpdateTrackDto } from './dto/track.dto';
+import { Track } from './interfaces/track.interface';
+import { TrackService } from './track.service';
+import {
+  Body,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+  Controller,
+  ParseUUIDPipe,
+} from '@nestjs/common';
 @Controller('track')
-export class TrackController {}
+export class TrackController {
+  constructor(private trackService: TrackService) {}
+
+  @Get()
+  async findAll(): Promise<Track[]> {
+    return this.trackService.findAll();
+  }
+
+  @Get(':uuid')
+  async getById(@Param('uuid', ParseUUIDPipe) uuid: string): Promise<Track> {
+    return this.trackService.getById(uuid);
+  }
+
+  @Post()
+  @HttpCode(201)
+  async create(@Body() dto: CreateTrackDto) {
+    return this.trackService.create(dto);
+  }
+
+  @Put(':uuid')
+  async update(
+    @Param('uuid', ParseUUIDPipe) uuid: string,
+    @Body() dto: UpdateTrackDto,
+  ): Promise<Track> {
+    return this.trackService.update(dto, uuid);
+  }
+
+  @Delete(':uuid')
+  @HttpCode(204)
+  async delete(@Param('uuid', ParseUUIDPipe) uuid: string): Promise<string> {
+    return this.trackService.delete(uuid);
+  }
+}
