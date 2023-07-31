99d27acbffd8c2d52a8fb5a9bfdc8424ca25100c
Polishchuk <staspol216@gmail.com>
2023-08-01 02:38:41 +0500

    feat: add swagger config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,13 @@ import { TrackModule } from './track/track.module';
 import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
 import { FavoriteModule } from './favorite/favorite.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     UserModule,
     TrackModule,
     DBModule,
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,12 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('Home Library')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/doc-json', app, document);
   await app.listen(4000);
 }
 bootstrap();
