5346a791f26fe207f56a385faf4bfaaa09056795
Polishchuk <staspol216@gmail.com>
2023-08-22 03:36:02 +0500

    fix: remove log

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -14,7 +14,6 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
       context.getHandler(),
       context.getClass(),
     ]);
-    console.log(isPublic, 'isPublic');
     if (isPublic) {
       return true;
     }
